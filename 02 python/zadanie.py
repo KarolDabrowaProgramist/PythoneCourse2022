1cec45ea5f5575b035febf63ed6a32a637a2a9d7
Karol <dabrowa.karol@gmail.com>
2022-05-12 20:10:29 +0200

    fixed

--- 02 python/zadanie.py
+++ 02 python/zadanie.py
@@ -1,6 +1,6 @@
 quote = 'Honesty is the first chapter in the book of wisdom.'
 
-new_qoute = qoute.replace('wisdom','friendship')
+new_quote = quote.replace('wisdom','friendship')
 print("1: Ilość znaków:", len(quote))
 print("2:", quote[-7:-1:1])
 print("3:", quote[:25:1])
